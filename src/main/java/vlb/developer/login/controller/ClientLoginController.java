a3a8d17f6f9e125e2be63da29ce27b4b49d95ab2
vlbota <viniciuslbota@gmail.com>
2026-05-17 17:10:57 -0300

    criando autenticação

--- src/main/java/vlb/developer/login/controller/ClientLoginController.java
+++ src/main/java/vlb/developer/login/controller/ClientLoginController.java
@@ -7,6 +7,8 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.NewCookie;
+import jakarta.ws.rs.core.Response;
 import vlb.developer.login.dtos.LoginResponseDto;
 import vlb.developer.login.forms.LoginForm;
 import vlb.developer.login.forms.RefreshTokenForm;
@@ -23,7 +25,20 @@ public class ClientLoginController {
     @POST
     @Path("/login")
     public LoginResponseDto login(@Valid LoginForm form) {
-        return loginService.login(form);
+        LoginResponseDto dto = loginService.login(form);
+        NewCookie refreshCookie = new NewCookie.Builder("rocketfinauth.refresh.token")
+                .value(dto.getRefreshToken())
+                .maxAge(60 * 60 * 24)  // 24 horas
+                .httpOnly(true)         // JS não consegue ler
+                .secure(true)           // só HTTPS
+                .sameSite(NewCookie.SameSite.STRICT)
+                .path("/")
+                .build();
+
+        // retorna o dto sem o refreshToken + cookie no header
+        return Response.ok(dto.semRefreshToken()).cookie(refreshCookie).build();
+
+//        return loginService.login(form);
     }
 
     @POST
--- src/main/java/vlb/developer/login/dtos/LoginResponseDto.java
+++ src/main/java/vlb/developer/login/dtos/LoginResponseDto.java
@@ -1,13 +1,16 @@
 package vlb.developer.login.dtos;
 
+import lombok.Getter;
+
 import java.util.UUID;
 
+@Getter
 public class LoginResponseDto {
 
-    public String token;
-    public String refreshToken;
-    public String name;
-    public UUID id;
+    private String token;
+    private String refreshToken;
+    private String name;
+    private UUID id;
 
     public LoginResponseDto(String token, String refreshToken, String name, UUID id) {
         this.token = token;
